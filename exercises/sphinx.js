ca4dcfbfa9ce661af10a84a9c3f7c063385ba766
Djavan Munroe <djavanm24@gmail.com>
2019-05-27 16:14:27 -0600

    Finish sphinx

--- exercises/sphinx.js
+++ exercises/sphinx.js
@@ -1,34 +1,44 @@
 class Sphinx {
   constructor() {
     this.riddles = [];
+    this.riddleCounter = 0;
     this.heroesEaten = 0;
-    this.wrongCounter = 2;
-    this.correctCounter = this.riddles.length;
- }
- collectRiddle(riddle) {
-   this.riddles.push(riddle);
-   if (this.riddles.length > 3) {
-     this.riddles.shift();
-   }
- }
-attemptAnswer(string){
-for(this.i=0;this.i<this.riddles.length;this.i++) {
-  if(string == this.riddles[this.i].answer) {
-    this.riddles.splice([this.i], 1)
-    this.wrongCounter--;
-    return 'That wasn\'t that hard, I bet you don\'t get the next one'
+
+  }
+  collectRiddle(riddle) {
+  if (this.riddles.length >= 3) {
+  this.riddles.shift();
+  this.riddles.push(riddle);
+  this.riddleCounter = 3;
   } else {
-    this.wrongCounter++
-  }
-  if(this.wrongCounter == 3) {
-    this.heroesEaten++
-  }
-  if(this.correctCounter == 0) {
-    return 'PSSSSSSS THIS HAS NEVER HAPPENED, HOW DID YOU KNOW THE ANSWER WAS \"Halfway, after that it\'s running out.\"???'
+    this.riddleCounter++;
+    this.riddles.push(riddle);
   }
 }
+  attemptAnswer(string) {
+    // console.log(this.riddleCounter);
+  for (var i = 0; i < this.riddles.length; i++) {
+    if(this.riddles[i].answer == string) {
+      this.riddles.splice([i], 1);
+      if(this.riddles.length >=1) {
+      return 'That wasn\'t that hard, I bet you don\'t get the next one'
+      }
+  } 
+  } 
+   if(this.riddles.length === this.riddleCounter) {
+    this.heroesEaten++;
 }
+   if(this.riddles.length === 0) {
+     return `PSSSSSSS THIS HAS NEVER HAPPENED, HOW DID YOU KNOW THE ANSWER WAS \"${string}\"???`
+   }
 }
+  // this.riddles.filter(function(riddle) {
+  //   return riddle.answer !== (string)
+  // })
+  // return this.riddles;
+  // }
+}
+
 module.exports = Sphinx;
 
 // npm test mythical-creatures/test/sphinx-test.js
